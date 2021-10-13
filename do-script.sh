61fdc87290af255fbc25e8f74b9fb080dce2b734
Juraj Ďurech <durech.juraj@gmail.com>
2021-10-13 12:59:52 +0200

    Convert tabs to spaces

--- do-script.sh
+++ do-script.sh
@@ -20,15 +20,15 @@ function VALIDATE_SCRIPT_PARAMS
 # -----------------------------------------------------------------------------
 function DO_DEPLOY
 {
-	local VER=$1
-	local DEPLOY_COMMAND=$2
-	
-	# validate variables and input parameters
-	VALIDATE_SCRIPT_PARAMS
-	
-	if [ "$DEPLOY_COMMAND" == "prepare" ]; then
-		
-		LOG "----- Validating..."
+    local VER=$1
+    local DEPLOY_COMMAND=$2
+    
+    # validate variables and input parameters
+    VALIDATE_SCRIPT_PARAMS
+    
+    if [ "$DEPLOY_COMMAND" == "prepare" ]; then
+        
+        LOG "----- Validating..."
         
         if [ ! -z "$DEPLOY_SCRIPT_PREPARE" ]; then
             local EVAL_COMMAND=${DEPLOY_SCRIPT_PREPARE/\%DEPLOY_VERSION\%/$VER}
@@ -37,18 +37,18 @@ function DO_DEPLOY
         else
             LOG "Skipping preparation step due to missing \$DEPLOY_SCRIPT_PREPARE variable in .limedeploy file."
         fi
-		
-	elif [ "$DEPLOY_COMMAND" == "deploy" ]; then
-		
-		LOG "----- Publishing..."
+        
+    elif [ "$DEPLOY_COMMAND" == "deploy" ]; then
+        
+        LOG "----- Publishing..."
         
         local EVAL_COMMAND=${DEPLOY_SCRIPT_DEPLOY/\%DEPLOY_VERSION\%/$VER}
         DEBUG_LOG "Going to execute 'eval $EVAL_COMMAND'"
         eval $EVAL_COMMAND
         
-	else
-		FAILURE "do-script.sh doesn't support '$DEPLOY_COMMAND' command"
-	fi
+    else
+        FAILURE "do-script.sh doesn't support '$DEPLOY_COMMAND' command"
+    fi
 }
 
 # -----------------------------------------------------------------------------
@@ -58,6 +58,6 @@ function DO_DEPLOY
 # -----------------------------------------------------------------------------
 function DO_PREPARE_TAG_MESSAGE
 {
-	VALIDATE_SCRIPT_PARAMS
-	DEPLOY_TAG_MESSAGE="Version $1"
+    VALIDATE_SCRIPT_PARAMS
+    DEPLOY_TAG_MESSAGE="Version $1"
 }
